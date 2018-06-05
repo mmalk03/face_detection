cfb318e20781ea3ad08a8bf6aa23f8bf5be1af8d
malkinskim <malkinskim@student.mini.pw.edu.pl>
2018-06-05 23:25:41 +0200

    Different hyperparemeters

--- yolo/reproduce_results.py
+++ yolo/reproduce_results.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 import pickle
 
-import matplotlib
-
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
+import numpy as np
 
-matplotlib.use('agg')
+# matplotlib.use('agg')
 
 plot_save_dir = 'report/plots/'
 history_dir = 'histories/'
 
 
 def main():
-    process('face_01_06_15-25')
+    hist1_name = 'face_01_06_15-25'
+    hist2_name = 'face_05_06_15-08'
+    # TODO: add even smaller input size and batch size = 8 or 16
+    process_history(hist1_name)
+    process_history(hist2_name)
+    compare_2_histories(hist1_name, hist2_name, '288x488, bs=2', '128x288, bs=4')
 
 
-def process(hist_name):
+def process_history(hist_name):
     hist = load_history(history_dir + hist_name)
     save_loss_history(hist, plot_save_dir + 'yolo_hist_loss_' + hist_name + '.png')
     save_map_history(history_dir + hist_name + '_mAP.txt', plot_save_dir + 'yolo_hist_map_' + hist_name + '.png')
 
 
 def load_history(path):
-    filename = open(path, "rb")
+    filename = open(path, 'rb')
     history = pickle.load(filename)
     filename.close()
     return history
@@ -36,15 +39,20 @@ def save_loss_history(hist, path):
     l3 = 'yolo layer 3 loss'
 
     plt.figure()
-    plt.plot(hist['loss'], label=l, color='lightcoral')
-    plt.plot(hist['yolo_layer_1_loss'], label=l1, color='cadetblue')
-    plt.plot(hist['yolo_layer_2_loss'], label=l2, color='darkslategray')
-    plt.plot(hist['yolo_layer_3_loss'], label=l3, color='crimson')
+    x = np.arange(-2, len(hist['loss']) - 2)
+    plt.plot(x, hist['loss'], label=l, color='lightcoral')
+    plt.plot(x, hist['yolo_layer_1_loss'], label=l1, color='cadetblue')
+    plt.plot(x, hist['yolo_layer_2_loss'], label=l2, color='darkslategray')
+    plt.plot(x, hist['yolo_layer_3_loss'], label=l3, color='crimson')
+
+    for var in (hist['loss'], hist['yolo_layer_1_loss'], hist['yolo_layer_2_loss'], hist['yolo_layer_3_loss']):
+        plt.annotate('%0.3f' % var[-1], xy=(1, var[-1]), xytext=(8, 0), va='center',
+                     xycoords=('axes fraction', 'data'), textcoords='offset points')
+
     plt.xlabel('Epoch')
-    plt.ylim(ymax=12, ymin=0)
-    plt.axvline(x=2, label='End of warm up training')
+    plt.ylim(ymax=10, ymin=0)
+    plt.axvline(x=0, label='End of warm up training', linestyle='dashed')
     plt.legend(loc='upper right')
-    plt.tight_layout()
     plt.savefig(path)
 
 
@@ -55,42 +63,88 @@ def save_map_history(map_path, save_path):
         str_list = numbers.split('\n')
         str_list.pop()
         float_list = [float(x) for x in str_list]
+        x = np.arange(-2, len(float_list) - 2)
         plt.figure()
-        plt.plot(float_list, label=l, color='lightcoral')
+        plt.plot(x, float_list, label=l, color='lightcoral')
+        plt.annotate('%0.3f' % float_list[-1], xy=(1, float_list[-1]), xytext=(8, 0), va='center',
+                     xycoords=('axes fraction', 'data'), textcoords='offset points')
         plt.xlabel('Epoch')
-        plt.axvline(x=2, label='End of warm up training')
+        plt.axvline(x=0, label='End of warm up training', linestyle='dashed')
         plt.legend(loc='lower right')
-        plt.tight_layout()
         plt.savefig(save_path)
 
 
-def compare_2_histories(hist1, hist2, label1, label2, path_acc, path_loss):
-    l1t = label1 + ' train'
-    l1v = label1 + ' validation'
-    l2t = label2 + ' train'
-    l2v = label2 + ' validation'
+def compare_2_histories(hist_name1, hist_name2, label1, label2):
+    hist1 = load_history(history_dir + hist_name1)
+    hist2 = load_history(history_dir + hist_name2)
+    path_map1 = history_dir + hist_name1 + '_mAP.txt'
+    path_map2 = history_dir + hist_name2 + '_mAP.txt'
+    path_loss = plot_save_dir + 'compare_loss_' + hist_name1 + '-' + hist_name2 + '.png'
+    map_save_path = plot_save_dir + 'compare_map_' + hist_name1 + '-' + hist_name2 + '.png'
+
+    h1_l = label1 + ' loss'
+    h1_l1 = label1 + ' yolo layer 1 loss'
+    h1_l2 = label1 + ' yolo layer 2 loss'
+    h1_l3 = label1 + ' yolo layer 3 loss'
+    h2_l = label2 + ' loss'
+    h2_l1 = label2 + ' yolo layer 1 loss'
+    h2_l2 = label2 + ' yolo layer 2 loss'
+    h2_l3 = label2 + ' yolo layer 3 loss'
+
+    n = min(len(hist1['loss']), len(hist2['loss']))
+    x = np.arange(-2, n - 2)
 
     plt.figure()
-    plt.plot(hist1['acc'], label=l1t, color='firebrick')
-    plt.plot(hist1['val_acc'], label=l1v, color='orangered')
-    plt.plot(hist2['acc'], label=l2t, color='springgreen')
-    plt.plot(hist2['val_acc'], label=l2v, color='forestgreen')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
-    plt.savefig(path_acc)
+    plt.plot(x, hist1['loss'][:n], label=h1_l, color='#f57c00', linestyle='-.')
+    plt.plot(x, hist1['yolo_layer_1_loss'][:n], label=h1_l1, color='#ffc947', linestyle='-.')
+    plt.plot(x, hist1['yolo_layer_2_loss'][:n], label=h1_l2, color='#ff9800', linestyle='-.')
+    plt.plot(x, hist1['yolo_layer_3_loss'][:n], label=h1_l3, color='#c66900', linestyle='-.')
+    plt.plot(x, hist2['loss'][:n], label=h2_l, color='#0288d1')
+    plt.plot(x, hist2['yolo_layer_1_loss'][:n], label=h2_l1, color='#67daff')
+    plt.plot(x, hist2['yolo_layer_2_loss'][:n], label=h2_l2, color='#03a9f4')
+    plt.plot(x, hist2['yolo_layer_3_loss'][:n], label=h2_l3, color='#007ac1')
 
-    plt.figure()
-    plt.plot(hist1['loss'], label=l1t, color='firebrick')
-    plt.plot(hist1['val_loss'], label=l1v, color='orangered')
-    plt.plot(hist2['loss'], label=l2t, color='springgreen')
-    plt.plot(hist2['val_loss'], label=l2v, color='forestgreen')
+    # for var in (
+    #         hist1['loss'][:n], hist1['yolo_layer_1_loss'][:n],
+    #         hist1['yolo_layer_2_loss'][:n], hist1['yolo_layer_3_loss'][:n],
+    #         hist2['loss'][:n], hist2['yolo_layer_1_loss'][:n],
+    #         hist2['yolo_layer_2_loss'][:n], hist2['yolo_layer_3_loss'][:n]):
+    #     plt.annotate('%0.3f' % var[-1], xy=(1, var[-1]), xytext=(8, 0), va='center',
+    #                  xycoords=('axes fraction', 'data'), textcoords='offset points')
+
     plt.xlabel('Epoch')
-    plt.ylabel('Loss')
+    plt.ylim(ymax=12, ymin=0)
+    plt.xlim(xmin=-3)
+    plt.axvline(x=0, label='End of warm up training', linestyle='dotted')
     plt.legend(loc='upper right')
-    plt.tight_layout()
     plt.savefig(path_loss)
+
+    h1_l = label1 + ' Mean Average Precision at 0.5'
+    h2_l = label2 + ' Mean Average Precision at 0.5'
+    with open(path_map1) as f1:
+        with open(path_map2) as f2:
+            numbers1 = f1.read()
+            str_list1 = numbers1.split('\n')
+            str_list1.pop()
+            float_list1 = [float(x) for x in str_list1]
+
+            numbers2 = f2.read()
+            str_list2 = numbers2.split('\n')
+            str_list2.pop()
+            float_list2 = [float(x) for x in str_list2]
+
+            plt.figure()
+            plt.plot(x, float_list1[:n], label=h1_l)
+            plt.plot(x, float_list2[:n], label=h2_l)
+
+            # for var in (float_list1[:n], float_list2[:n]):
+            #     plt.annotate('%0.3f' % var[-1], xy=(1, var[-1]), xytext=(8, 0), va='center',
+            #                  xycoords=('axes fraction', 'data'), textcoords='offset points')
+
+            plt.xlabel('Epoch')
+            plt.axvline(x=0, label='End of warm up training', linestyle='dotted')
+            plt.legend(loc='lower right')
+            plt.savefig(map_save_path)
 
 
 if __name__ == '__main__':
